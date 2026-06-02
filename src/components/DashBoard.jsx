da2e64d0a2b4868fd34723928cb4fcb796948be9
Safvan P <pcsajitha87@gmail.com>
2026-06-02 16:23:26 +0530

    completed

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./Navbar";
+import Navbar from "../components/NavBar.jsx";
 
 export default function DashBoard() {
   const [employees, setEmployees] = useState([]);
